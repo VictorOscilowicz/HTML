f458c16512c2f1a5552e5b1d7ea880ac4d5a9ad3
VictorOscilowicz <7voscilowicz@stpatrick.org>
2017-03-21 10:47:28 -0500

    Add files via upload

---
+++ click.js
@@ -0,0 +1,16 @@
+//declare samecolor() here
+function samecolor(hex){
+  let hex=
+
+  for(let i=0;i<section.samecolor.length;i++){
+
+  }
+}
+//declare diffcolor() here
+function diffcolor(hex){
+  let hex=
+
+  for(let i=0;i<section.diffcolor.length;i++){
+
+  }
+}
---
+++ resize.js
@@ -0,0 +1,9 @@
+//Declare tabletSize() here
+function tabletSize(){
+
+}
+
+//Declare phoneSize() here
+function phoneSize(){
+  
+}
