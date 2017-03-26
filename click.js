1ca53b3410dadf39ece45aa40c15d06d3222d476
VictorOscilowicz <7voscilowicz@stpatrick.org>
2017-03-26 16:11:17 -0500

    Add files via upload

--- click.js
+++ click.js
@@ -1,19 +1,20 @@
 //declare samecolor() here
 function samecolor(){
-  let newcolor = "";
-  let
+  let newcolor = "#FF3C33";
+  let selection = document.selectorQueryAll(div);
 
-  for(let i=0;i<section.div;i++){
-    document.div=
-    document.div=
+  for(let i=0;i<document.div;i++){
+    document.div.backgroudColor=newcolor;
+    document.div.height="300px";
+    document.div.opacity=math.random();
 
   }
 }
 //declare diffcolor() here
 function diffcolor(){
-  let color = "";
+  let color = "334CFF";
 
-  for(let i=0;i<section.diffcolor.length;i++){
+  for(let p=0;p<section.diffcolor.length;p++){
 
   }
 }
